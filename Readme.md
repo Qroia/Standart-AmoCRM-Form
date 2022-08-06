4d2221a423a5841939f738db639e1ffbef3c0f92
Qroia FAK(C)E <dannywhatson@protonmail.com>
2022-08-06 19:24:40 +0300

    SF-AMOCRM 1.5
    Beatiful form(Tailwind)
    Min.Validation JS values

---
+++ Readme.md
@@ -0,0 +1,13 @@
+### Standart Form + AmoCRM API(curl)
+
+- Technology:
+- - JQuery
+- - TailwindCSS
+- - PHP 8.0
+- - HTML, JS
+
+Standard form based on HTML and Jquery. CDN Tailwind is used as styling.
+
+Minimal Validation
+PHP 8.0+
+curl Requests
--- js/index.js
+++ js/index.js
@@ -1,17 +1,32 @@
-$('#sendBtn').on('click', () => {
-    var email = $('#emailId').val();
-    var name = $('#nameId').val();
-    var pn = $('#phoneId').val();
-    var city = $('#cityId').val();
-    var service = $('#serviceId').val();
-    var msg = $('#messageId').val();
+$('#send-btn-form').on('click', () => {
+    const email = $('#email-form').val().replace(/\s+/g,' ');
+    const firstName = $('#grid-first-name').val().replace(/\s+/g,' ');
+    const lastName = $('#grid-last-name').val().replace(/\s+/g,' ');
+    const pn = $('#phn-form').val().replace(/\s+/g,' ');
+    const city = $('#city-form').val().replace(/\s+/g,' ');
+    const service = $('#service-from').val();
+    const msg = $('#msg-form').val();
+
+    const email_regex = /.*@.*\..*/gm;
+
+    // Email Validation. Min.Email: name(min length: 1), Min Domain(2), min NameDomain(2)
+    if (!(email_regex.test(email) || email.length < 7)) {
+        alert('Неправильный(ая) номер телефона или почта');
+        return false;
+    }
+
+    // Length Validation
+    if (firstName.length < 2 || lastName.length < 2 || pn.length < 5 || city.length < 3 || service == '' || msg.length < 10) {
+        alert('Введите все поля!');
+        return false;
+    }
 
     $.post('./php/index.php', {
-            'email': email,
-            'name': name,
-            'phone': pn,
-            'city': city,
-            'service': service,
-            'msg': msg
-        });
+        'email': email,
+        'name': `${firstName} ${lastName}`,
+        'phone': pn,
+        'city': city,
+        'service': service,
+        'msg': msg
+    });
 })
